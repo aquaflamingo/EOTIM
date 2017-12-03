7450a7f80fc2e1819b0adaaa8ea406329b204bbb
robertsimoes <dev.robertsimoes@gmail.com>
2017-12-03 15:57:24 -0500

    add dashboard styling

--- src/layouts/dashboard/Dashboard.js
+++ src/layouts/dashboard/Dashboard.js
@@ -8,14 +8,36 @@ class Dashboard extends Component {
 
   render() {
     return(
-      <main className="container">
-        <div className="pure-g">
-          <div className="pure-u-1-1">
-            <h1>Dashboard</h1>
-            <p><strong>Congratulations {this.props.authData.name}!</strong> If you're seeing this page, you've logged in with your own smart contract successfully.</p>
+      <div className="container">
+        <br/>
+        <br/>
+        <div className="columns">
+          <div className="column is-3">
+            <aside class="menu">
+              <p class="menu-label">
+                General
+              </p>
+              <ul class="menu-list">
+                <li><a class="is-active">Dashboard</a></li>
+                <li><a>Open Market</a></li>
+              </ul>
+              <br/>
+              <p class="menu-label">
+                Announcements
+              </p>
+              <ul class="menu-list">
+                <li><a>We launched 🎊!</a></li>
+              </ul>
+            </aside>
           </div>
+       
+        <div className="column is-9">
+          <h1 className="title">Dashboard</h1>
+          <p className="subtitle"><strong>Congratulations {this.props.authData.name}!</strong> If you're seeing this page, you've logged in with your own smart contract successfully.</p>
         </div>
-      </main>
+
+      </div>
+      </div>
     )
   }
 }
